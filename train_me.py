6e960295880d5aa9d4c686a456526f389bf14772
faheinrich <faheinrich98@gmail.com>
2022-12-08 18:23:07 +0100

    Cleaned up train script, fixed 6000 vocab file

--- train_me.py
+++ train_me.py
@@ -1,108 +1,88 @@
-import numpy as np
 import pandas as pd
 import random
 import sys
-import glob
 
 
+def print_vocab(current_vocab: dict, lang_mode: str):
+    def ask_deutsch():
+        print(current_vocab["deutsch"], end="")
+        input("")
+        print(current_vocab["hiragana"], current_vocab["kanji"], "\n")
 
-# printed je nach Flags eine zufällige Vokabel aus und wartet auf Enter
-def print_vocab(current_vocab, lang_mode):
+    def ask_japanisch():
+        # if current_vocab["kanji"] != "":
+        #     print(current_vocab["hiragana"], ", ", current_vocab["kanji"], end="")
+        # else:
+        print(current_vocab["hiragana"], end="")
+        input("")
+        print(current_vocab["deutsch"], "\n")
 
     if lang_mode == "japanisch":
-        print(current_vocab[0], end="")
-        input("")
-        print(current_vocab[2], "\n")
+        ask_japanisch()
     elif lang_mode == "deutsch":
-        print(current_vocab[2], end="")
-        input("")
-        print(current_vocab[0], "\n")
+        ask_deutsch()
     elif lang_mode == "mix": 
-        if(bool(random.getrandbits(1))):
-            print(current_vocab[0], end="")
-            input("")
-            print(current_vocab[2], "\n")
+        if random.random() > 0.5:
+            ask_japanisch()
         else:
-            print(current_vocab[2], end="")
-            input("")
-            print(current_vocab[0], "\n")
+            ask_japanisch()
 
-
+def print_scorebreak(idx):
+    br = '============'
+    mid = '=' * (len(str(idx)) + 2)
+    print(f"\n{br}{mid}{br}\n{br} {idx} {br}\n{br}{mid}{br}\n\n")
 
 def main():
-    try:
-        if sys.argv[1] in {"japanisch", "deutsch", "mix"}:
+    lang_mode = None
+    if len(sys.argv) > 1:
             lang_mode = sys.argv[1]
-    except:
-        language_given = False
-        while not language_given:
-            lang_mode = input("Sprache eingeben [japanisch | deutsch | mix]: ")
-            if lang_mode in {"japanisch", "deutsch", "mix"}:
-                language_given = True
-        
-    # entweder alle Dateien im Ordner suchen oder eigene Auswahl benutzen
-    all_files = False
-    nur_verben = False
+    while lang_mode is None:
+        user_input = input("Sprache eingeben [japanisch | deutsch | mix]: ")
+        if user_input in {"japanisch", "deutsch", "mix"}:
+            lang_mode = user_input
 
-    if all_files:
-        # alle Dateien in ./vocab_data suchen
-        file_paths = glob.glob("vocab_data/*")
-    else:
-        # eigene Auswahl an Dateien
-        file_paths = []
-
-        file_paths.append("vocab_data/A1T1.xlsm")
-
-        file_paths.append("vocab_data/Vok1.xlsx")
-        file_paths.append("vocab_data/Vok2.xlsx")
-        file_paths.append("vocab_data/Vok3.xlsx")
-        file_paths.append("vocab_data/Vok4.xlsx")
-        file_paths.append("vocab_data/Vok5.xlsx")
-        file_paths.append("vocab_data/Vok6.xlsx")
-        
-        file_paths.append("vocab_data/Zaehlen.xlsx")
-        file_paths.append("vocab_data/Verben.xlsx")
-        
-        file_paths.append("vocab_data/mehr_verben.xlsx")
-
+    file_paths = []
+    file_paths.append("vocab_data/A1T1.xlsx")
+    file_paths.append("vocab_data/Vok1.xlsx")
+    file_paths.append("vocab_data/Vok2.xlsx")
+    file_paths.append("vocab_data/Vok3.xlsx")
+    file_paths.append("vocab_data/Vok4.xlsx")
+    file_paths.append("vocab_data/Vok5.xlsx")
+    file_paths.append("vocab_data/Vok6.xlsx")
+    file_paths.append("vocab_data/Zaehlen.xlsx")
+    file_paths.append("vocab_data/Verben.xlsx")
+    file_paths.append("vocab_data/mehr_verben.xlsx")
+    file_paths.append("vocab_data/noch_mehr_verben.xlsx")
 
     vocab_filtered = []
 
-    print("Lade Vokabeln,")
-    # alle Dateien in file_paths einlesen
+    print("Lade Vokabeln...")
     for fp in file_paths:
-        # print(fp)
-        # Excel Datei als pandas-dataframe einlesen
         df = pd.read_excel(fp)
+        assert df.shape[1] == 3, "Too many columns in file."
+        for idx, hiragana, kanji, deutsch in df.itertuples():
 
-        # dataframe zu np array
-        vocab_raw = df.to_numpy()
+            if not isinstance(hiragana, str) or not isinstance(deutsch, str):
+                continue
+            if not isinstance(kanji, str):
+                kanji = ""
 
-        # alles aussortieren, was nicht alle werte der vokabeln ausfuellt
-        for c, i in enumerate(vocab_raw):
-            # gibt fehler wenn ein feld leer ist, landet somit im except Fall
-            try:
-                if(np.isnan(np.float(i[2]))):
-                    0
-            except:
-                vocab_filtered.append(i)
+            vocab_dict = {"hiragana": hiragana,
+                          "kanji": kanji,
+                          "deutsch": deutsch}
+            vocab_filtered.append(vocab_dict)
 
     num_vocab = len(vocab_filtered)
-    print(num_vocab, "Vokabeln geladen.\n")
-
-    batch_size = 20
+    print(f"{num_vocab} Vokabeln geladen.\n")
     
-    for i in range(1, 1000000000):
+    vocab_count = 0
+    while True:
+        vocab_dict = random.choice(vocab_filtered)
+        print_vocab(vocab_dict, lang_mode)
 
-        for index in np.random.choice(num_vocab, batch_size):
-            print_vocab(vocab_filtered[index], lang_mode)	
-
-        
-        br = '============'
-        c = i * batch_size
-        mid = '='*(len(str(c))+2)
-        print(f"\n{br}{mid}{br}\n{br} {c} {br}\n{br}{mid}{br}\n\n")
-
+        if vocab_count % 20 == 0 and vocab_count > 0:
+            print_scorebreak(vocab_count)
+        vocab_count += 1
     
 
 if __name__ == "__main__":
